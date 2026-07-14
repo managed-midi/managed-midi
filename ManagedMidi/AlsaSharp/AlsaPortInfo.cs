88147221be0a443a5731b0c4fe028c944c7dba25
Jon Skeet <skeet@pobox.com>
2026-07-14 11:33:54 +0100

    AlsaSharp: Adjust namespace, use file-scoped namespace declarations, and reformat

--- ManagedMidi/AlsaSharp/AlsaPortInfo.cs
+++ ManagedMidi/AlsaSharp/AlsaPortInfo.cs
@@ -1,139 +1,154 @@
 ﻿using System;
 using System.Runtime.InteropServices;
 
-namespace AlsaSharp {
-	public class AlsaPortInfo : IDisposable {
-		public const int PortSystemTimer = 0;
-		public const int PortSystemAnnouncement = 1;
+namespace ManagedMidi.AlsaSharp;
 
-		static IntPtr Malloc ()
-		{
-			unsafe {
-				var ptr = IntPtr.Zero;
-				var pref = &ptr;
-				Natives.snd_seq_port_info_malloc ((IntPtr)pref);
-				return ptr;
-			}
-		}
+public class AlsaPortInfo : IDisposable
+{
+    public const int PortSystemTimer = 0;
+    public const int PortSystemAnnouncement = 1;
 
-		static void Free (IntPtr handle)
-		{
-			if ((IntPtr)handle != IntPtr.Zero)
-				Natives.snd_seq_port_info_free (handle);
-		}
+    static IntPtr Malloc()
+    {
+        unsafe
+        {
+            var ptr = IntPtr.Zero;
+            var pref = &ptr;
+            Natives.snd_seq_port_info_malloc((IntPtr) pref);
+            return ptr;
+        }
+    }
 
-		public AlsaPortInfo ()
-			: this (Malloc (), Free)
-		{
-		}
+    static void Free(IntPtr handle)
+    {
+        if ((IntPtr) handle != IntPtr.Zero)
+            Natives.snd_seq_port_info_free(handle);
+    }
 
-		public AlsaPortInfo (IntPtr handle, int port)
-		{
-			this.handle = handle;
-			this.free = _ => {};
-		}
+    public AlsaPortInfo()
+        : this(Malloc(), Free)
+    {
+    }
 
-		public AlsaPortInfo (IntPtr handle, Action<IntPtr> free)
-		{
-			this.handle = handle;
-			this.free = free;
-		}
+    public AlsaPortInfo(IntPtr handle, int port)
+    {
+        this.handle = handle;
+        this.free = _ => { };
+    }
 
-		Pointer<snd_seq_port_info_t> handle;
-		Action<IntPtr> free;
+    public AlsaPortInfo(IntPtr handle, Action<IntPtr> free)
+    {
+        this.handle = handle;
+        this.free = free;
+    }
 
-		internal IntPtr Handle => handle;
+    Pointer<snd_seq_port_info_t> handle;
+    Action<IntPtr> free;
 
-		public void Dispose ()
-		{
-			if (name_ptr != IntPtr.Zero)
-				Marshal.FreeHGlobal (name_ptr);
-			if ((IntPtr)handle != IntPtr.Zero)
-				free (handle);
-			handle = IntPtr.Zero;
-		}
+    internal IntPtr Handle => handle;
 
-		public AlsaPortInfo Clone ()
-		{
-			var ret = new AlsaPortInfo ();
-			Natives.snd_seq_port_info_copy (ret.Handle, Handle);
-			return ret;
-		}
+    public void Dispose()
+    {
+        if (name_ptr != IntPtr.Zero)
+            Marshal.FreeHGlobal(name_ptr);
+        if ((IntPtr) handle != IntPtr.Zero)
+            free(handle);
+        handle = IntPtr.Zero;
+    }
 
-		public int Client {
-			get => Natives.snd_seq_port_info_get_client (handle);
-			set => Natives.snd_seq_port_info_set_client (handle, value);
-		}
+    public AlsaPortInfo Clone()
+    {
+        var ret = new AlsaPortInfo();
+        Natives.snd_seq_port_info_copy(ret.Handle, Handle);
+        return ret;
+    }
 
-		public int Port {
-			get => Natives.snd_seq_port_info_get_port (handle);
-			set => Natives.snd_seq_port_info_set_port (handle, value);
-		}
+    public int Client
+    {
+        get => Natives.snd_seq_port_info_get_client(handle);
+        set => Natives.snd_seq_port_info_set_client(handle, value);
+    }
 
-		IntPtr name_ptr;
-		public string Name {
-			get => Marshal.PtrToStringAnsi (Natives.snd_seq_port_info_get_name (handle));
-			set {
-				if (name_ptr != IntPtr.Zero)
-					Marshal.FreeHGlobal (name_ptr);
-				name_ptr = Marshal.StringToHGlobalAnsi (value);
-				Natives.snd_seq_port_info_set_name (handle, name_ptr);
-			}
-		}
+    public int Port
+    {
+        get => Natives.snd_seq_port_info_get_port(handle);
+        set => Natives.snd_seq_port_info_set_port(handle, value);
+    }
 
-		public AlsaPortCapabilities Capabilities {
-			get => (AlsaPortCapabilities)Natives.snd_seq_port_info_get_capability (handle);
-			set => Natives.snd_seq_port_info_set_capability (handle, (uint)value);
-		}
+    IntPtr name_ptr;
+    public string Name
+    {
+        get => Marshal.PtrToStringAnsi(Natives.snd_seq_port_info_get_name(handle));
+        set
+        {
+            if (name_ptr != IntPtr.Zero)
+                Marshal.FreeHGlobal(name_ptr);
+            name_ptr = Marshal.StringToHGlobalAnsi(value);
+            Natives.snd_seq_port_info_set_name(handle, name_ptr);
+        }
+    }
 
-		public AlsaPortType PortType {
-			get => (AlsaPortType)Natives.snd_seq_port_info_get_type (handle);
-			set => Natives.snd_seq_port_info_set_type (handle, (uint)value);
-		}
+    public AlsaPortCapabilities Capabilities
+    {
+        get => (AlsaPortCapabilities) Natives.snd_seq_port_info_get_capability(handle);
+        set => Natives.snd_seq_port_info_set_capability(handle, (uint) value);
+    }
 
-		public int MidiChannels {
-			get => Natives.snd_seq_port_info_get_midi_channels (handle);
-			set => Natives.snd_seq_port_info_set_midi_channels (handle, value);
-		}
+    public AlsaPortType PortType
+    {
+        get => (AlsaPortType) Natives.snd_seq_port_info_get_type(handle);
+        set => Natives.snd_seq_port_info_set_type(handle, (uint) value);
+    }
 
-		public int MidiVoices {
-			get => Natives.snd_seq_port_info_get_midi_voices (handle);
-			set => Natives.snd_seq_port_info_set_midi_voices (handle, value);
-		}
+    public int MidiChannels
+    {
+        get => Natives.snd_seq_port_info_get_midi_channels(handle);
+        set => Natives.snd_seq_port_info_set_midi_channels(handle, value);
+    }
 
-		public int SynthVoices {
-			get => Natives.snd_seq_port_info_get_synth_voices (handle);
-			set => Natives.snd_seq_port_info_set_synth_voices (handle, value);
-		}
+    public int MidiVoices
+    {
+        get => Natives.snd_seq_port_info_get_midi_voices(handle);
+        set => Natives.snd_seq_port_info_set_midi_voices(handle, value);
+    }
 
-		public int ReadSubscriptions => Natives.snd_seq_port_info_get_read_use (handle);
+    public int SynthVoices
+    {
+        get => Natives.snd_seq_port_info_get_synth_voices(handle);
+        set => Natives.snd_seq_port_info_set_synth_voices(handle, value);
+    }
 
-		public int WriteSubscriptions => Natives.snd_seq_port_info_get_write_use (handle);
+    public int ReadSubscriptions => Natives.snd_seq_port_info_get_read_use(handle);
 
-		public bool PortSpecified {
-			get => Natives.snd_seq_port_info_get_port_specified (handle) > 0;
-			set => Natives.snd_seq_port_info_set_port_specified (handle, value ? 1 : 0);
-		}
+    public int WriteSubscriptions => Natives.snd_seq_port_info_get_write_use(handle);
 
-		public int TimestampQueue {
-			get => Natives.snd_seq_port_info_get_timestamp_queue (handle);
-			set => Natives.snd_seq_port_info_set_timestamp_queue (handle, value);
-		}
+    public bool PortSpecified
+    {
+        get => Natives.snd_seq_port_info_get_port_specified(handle) > 0;
+        set => Natives.snd_seq_port_info_set_port_specified(handle, value ? 1 : 0);
+    }
 
-		public int TimestampReal {
-			get => Natives.snd_seq_port_info_get_timestamp_real (handle);
-			set => Natives.snd_seq_port_info_set_timestamp_real (handle, value);
-		}
+    public int TimestampQueue
+    {
+        get => Natives.snd_seq_port_info_get_timestamp_queue(handle);
+        set => Natives.snd_seq_port_info_set_timestamp_queue(handle, value);
+    }
 
-		public bool Timestamping {
-			get => Natives.snd_seq_port_info_get_timestamping (handle) != 0;
-			set => Natives.snd_seq_port_info_set_timestamping (handle, value ? 1 : 0);
-		}
+    public int TimestampReal
+    {
+        get => Natives.snd_seq_port_info_get_timestamp_real(handle);
+        set => Natives.snd_seq_port_info_set_timestamp_real(handle, value);
+    }
 
-		public string Id => Client.ToString () + '_' + Port.ToString ();
+    public bool Timestamping
+    {
+        get => Natives.snd_seq_port_info_get_timestamping(handle) != 0;
+        set => Natives.snd_seq_port_info_set_timestamping(handle, value ? 1 : 0);
+    }
 
-		public string Manufacturer => string.Empty; // FIXME: implement
+    public string Id => Client.ToString() + '_' + Port.ToString();
 
-		public string Version => string.Empty; // FIXME: implement
-	}
+    public string Manufacturer => string.Empty; // FIXME: implement
+
+    public string Version => string.Empty; // FIXME: implement
 }
--- ManagedMidi/AlsaSharp/AlsaSystemInfo.cs
+++ ManagedMidi/AlsaSharp/AlsaSystemInfo.cs
@@ -1,36 +1,38 @@
 ﻿using System;
 
-namespace AlsaSharp {
-	public class AlsaSystemInfo : IDisposable {
-		public AlsaSystemInfo ()
-		{
-			IntPtr ptr = IntPtr.Zero;
-			unsafe {
-				var pref = &ptr;
-				Natives.snd_seq_system_info_malloc ((IntPtr)pref);
-				handle = ptr;
-			}
-		}
+namespace ManagedMidi.AlsaSharp;
 
-		public void SetContextSequencer (AlsaSequencer seq)
-		{
-			Natives.snd_seq_system_info (seq.SequencerHandle, handle);
-		}
+public class AlsaSystemInfo : IDisposable
+{
+    public AlsaSystemInfo()
+    {
+        IntPtr ptr = IntPtr.Zero;
+        unsafe
+        {
+            var pref = &ptr;
+            Natives.snd_seq_system_info_malloc((IntPtr) pref);
+            handle = ptr;
+        }
+    }
 
-		public int MaxQueueCount => Natives.snd_seq_system_info_get_queues (handle);
-		public int MaxClientCount => Natives.snd_seq_system_info_get_clients (handle);
-		public int PortCount => Natives.snd_seq_system_info_get_ports (handle);
-		public int ChannelCount => Natives.snd_seq_system_info_get_channels (handle);
-		public int CurrentQueueCount => Natives.snd_seq_system_info_get_cur_queues (handle);
-		public int CurrentClientCount => Natives.snd_seq_system_info_get_cur_clients (handle);
+    public void SetContextSequencer(AlsaSequencer seq)
+    {
+        Natives.snd_seq_system_info(seq.SequencerHandle, handle);
+    }
 
-		Pointer<snd_seq_system_info_t> handle;
+    public int MaxQueueCount => Natives.snd_seq_system_info_get_queues(handle);
+    public int MaxClientCount => Natives.snd_seq_system_info_get_clients(handle);
+    public int PortCount => Natives.snd_seq_system_info_get_ports(handle);
+    public int ChannelCount => Natives.snd_seq_system_info_get_channels(handle);
+    public int CurrentQueueCount => Natives.snd_seq_system_info_get_cur_queues(handle);
+    public int CurrentClientCount => Natives.snd_seq_system_info_get_cur_clients(handle);
 
-		public void Dispose ()
-		{
-			if ((IntPtr)handle != IntPtr.Zero)
-				Natives.snd_seq_system_info_free (handle);
-			handle = IntPtr.Zero;
-		}
-	}
+    Pointer<snd_seq_system_info_t> handle;
+
+    public void Dispose()
+    {
+        if ((IntPtr) handle != IntPtr.Zero)
+            Natives.snd_seq_system_info_free(handle);
+        handle = IntPtr.Zero;
+    }
 }
