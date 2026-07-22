f63bed3ca59b951458c13153ea3c85f1c1a4b19a
Jon Skeet <skeet@pobox.com>
2026-07-22 08:53:43 +0100

    Move track merging functionality into MidiMusic

--- ManagedMidi/Smf/MidiMusic.cs
+++ ManagedMidi/Smf/MidiMusic.cs
@@ -17,11 +17,9 @@ public class MidiMusic
 
     public void AddTrack(MidiTrack track) => Tracks.Add(track);
 
-
-    public IEnumerable<MidiMessage> GetMetaEventsOfType(byte metaType) =>
-        Format != 0
-        ? SmfTrackMerger.Merge(this).GetMetaEventsOfType(metaType)
-        : GetMetaEventsOfType(Tracks[0].Messages, metaType);
+    // Note: the methods below which call GetMergedMessages() are cheap when the format is 0,
+    // GetMergedMessages() jjust returns "this".
+    public IEnumerable<MidiMessage> GetMetaEventsOfType(byte metaType) => GetMetaEventsOfType(GetMergedMessages().Tracks[0].Messages, metaType);
 
     public static IEnumerable<MidiMessage> GetMetaEventsOfType(IEnumerable<MidiMessage> messages, byte metaType)
     {
@@ -36,20 +34,9 @@ public class MidiMusic
         }
     }
 
-    public int GetTotalTicks() =>
-        Format != 0
-        ? SmfTrackMerger.Merge(this).GetTotalTicks()
-        : Tracks[0].Messages.Sum(m => m.DeltaTime);
-
-    public int GetTotalPlayTimeMilliseconds() =>
-        Format != 0
-        ? SmfTrackMerger.Merge(this).GetTotalPlayTimeMilliseconds()
-        : GetTotalPlayTimeMilliseconds(Tracks[0].Messages, DeltaTimeSpec);
-
-    public int GetTimePositionInMillisecondsForTick(int ticks) =>
-        Format != 0
-        ? SmfTrackMerger.Merge(this).GetTimePositionInMillisecondsForTick(ticks)
-        : GetPlayTimeMillisecondsAtTick(Tracks[0].Messages, ticks, DeltaTimeSpec);
+    public int GetTotalTicks() => GetMergedMessages().Tracks[0].Messages.Sum(m => m.DeltaTime);
+    public int GetTotalPlayTimeMilliseconds() => GetTotalPlayTimeMilliseconds(GetMergedMessages().Tracks[0].Messages, DeltaTimeSpec);
+    public int GetTimePositionInMillisecondsForTick(int ticks) => GetPlayTimeMillisecondsAtTick(GetMergedMessages().Tracks[0].Messages, ticks, DeltaTimeSpec);
 
     public static int GetTotalPlayTimeMilliseconds(IList<MidiMessage> messages, int deltaTimeSpec) =>
         GetPlayTimeMillisecondsAtTick(messages, messages.Sum(m => m.DeltaTime), deltaTimeSpec);
@@ -78,5 +65,92 @@ public class MidiMusic
             }
         }
         return (int) v;
+    }
+
+    internal MidiMusic GetMergedMessages()
+    {
+        if (Format == 0)
+        {
+            return this;
+        }
+
+        IList<MidiMessage> l = new List<MidiMessage>();
+
+        foreach (var track in Tracks)
+        {
+            int delta = 0;
+            foreach (var mev in track.Messages)
+            {
+                delta += mev.DeltaTime;
+                l.Add(new MidiMessage(delta, mev.Event));
+            }
+        }
+
+        if (l.Count == 0)
+        {
+            return new MidiMusic() { DeltaTimeSpec = DeltaTimeSpec }; // empty (why did you need to sort your song file?)
+        }
+
+        // Usual Sort() over simple list of MIDI events does not work as expected.
+        // For example, it does not always preserve event 
+        // orders on the same channels when the delta time
+        // of event B after event A is 0. It could be sorted
+        // either as A->B or B->A.
+        //
+        // To resolve this issue, we have to sort "chunk"
+        // of events, not all single events themselves, so
+        // that order of events in the same chunk is preserved
+        // i.e. [AB] at 48 and [CDE] at 0 should be sorted as
+        // [CDE] [AB].
+
+        var idxl = new List<int>(l.Count);
+        idxl.Add(0);
+        int prev = 0;
+        for (int i = 0; i < l.Count; i++)
+        {
+            if (l[i].DeltaTime != prev)
+            {
+                idxl.Add(i);
+                prev = l[i].DeltaTime;
+            }
+        }
+
+        idxl.Sort(delegate (int i1, int i2)
+        {
+            return l[i1].DeltaTime - l[i2].DeltaTime;
+        });
+
+        // now build a new event list based on the sorted blocks.
+        var l2 = new List<MidiMessage>(l.Count);
+        int idx;
+        for (int i = 0; i < idxl.Count; i++)
+        {
+            for (idx = idxl[i], prev = l[idx].DeltaTime; idx < l.Count && l[idx].DeltaTime == prev; idx++)
+            {
+                l2.Add(l[idx]);
+            }
+        }
+        l = l2;
+
+        // now messages should be sorted correctly.
+
+        var waitToNext = l[0].DeltaTime;
+        for (int i = 0; i < l.Count - 1; i++)
+        {
+            if (l[i].Event.Value != 0)
+            { // if non-dummy
+                var tmp = l[i + 1].DeltaTime - l[i].DeltaTime;
+                l[i] = new MidiMessage(waitToNext, l[i].Event);
+                waitToNext = tmp;
+            }
+        }
+        l[l.Count - 1] = new MidiMessage(waitToNext, l[l.Count - 1].Event);
+
+        return new MidiMusic
+        {
+            DeltaTimeSpec = DeltaTimeSpec,
+            Format = 0,
+            Tracks = { new MidiTrack(l) }
+        };
     }
 }
